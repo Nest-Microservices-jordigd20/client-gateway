453b0a071888d2747d22bd9a896b5c50bbfa1493
jordigd20 <jordigd8@gmail.com>
2024-08-19 19:17:01 +0200

    feat: add products controller

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
+import { Type } from "class-transformer";
+
+export class CreateProductDto {
+
+  @IsString()
+  @IsNotEmpty()
+  public name: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2
+  })
+  @Min(0)
+  @Type(() => Number)
+  public price: number;
+  
+}

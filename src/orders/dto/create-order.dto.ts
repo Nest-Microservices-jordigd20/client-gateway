eb89d78de3f33874ca520d386d0ef778a25cdb52
jordigd20 <jordigd8@gmail.com>
2024-08-22 12:26:36 +0200

    feat: update CreateOrderDto to use OrderItemDto for items array validation

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,21 +1,11 @@
-import { IsEnum, IsNumber, IsOptional, IsPositive } from 'class-validator';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
-import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  @Type(() => Number)
-  totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  @Type(() => Number)
-  totalItems: number;
-
-  @IsEnum(OrderStatusList, {
-    message: `Possible status values are ${OrderStatusList.join(', ')}`
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
